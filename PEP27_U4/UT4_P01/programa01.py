1ada5775ccb176026a0ad472f82d04b06c8d1723
David <davidgallegs11@gmail.com>
2025-11-25 09:22:18 +0100

    Añade U4

---
+++ PEP27_U4/UT4_P01/programa01.py
@@ -0,0 +1,13 @@
+cadena1 = 'Buenos días'
+cadena2 = "Buenos días"
+cadena3 = """Buenos días"""
+
+print(f"""--- Cadenas ---
+Cadena 1: {cadena1}
+Cadena 2: {cadena2}
+Cadena 3: {cadena3}""")
+
+print(f"""--- Cadenas inicio y final ---
+Cadena 1: {cadena1[0]} {cadena1[-1]}
+Cadena 2: {cadena2[0]} {cadena2[-1]}
+Cadena 3: {cadena3[0]} {cadena3[-1]}""")
